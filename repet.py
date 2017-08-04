53ea3ef28bb6d462858228a12040d6abbb3d937c
Zafar <zafarrafii@gmail.com>
2017-08-04 12:02:42 -0700

    Added repet.py (module!)

--- repet.py
+++ repet.py
@@ -1,9 +1,28 @@
-class Repet:
-    """REPET Python class"""
+# Public functions
+def repet(audio_signal, sample_rate):
+    """REPET (original)
 
-    def __init__(self):
-        self.data = []
+    The original REPET aims at identifying and extracting the repeating patterns in an audio mixture,
+    by estimating a period of the underlying repeating structure and modeling a segment of the
+    periodically repeating background.
+    background_signal = repet.original(audio_signal,sample_rate);
+    """
 
-    def
+    background_signal = audio_signal + sample_rate
+    return background_signal
 
-        """Use MODULE"""
+
+# Private functions
+def _nextpow2(input_value):
+    """Exponent of next higher power of 2 (from https://wiki.python.org/moin/NumericAndScientificRecipes)"""
+    output_value = 1
+    while output_value < input_value:
+        output_value *= 2
+    return output_value
+
+
+def _stftparameters(window_duration, sample_rate):
+    """"STFT parameters (for constant overlap-add)"""
+
+    # Window length in samples (power of 2 for fast FFT)
+    window_length = pow(2, _nextpow2(window_duration*sample_rate))
